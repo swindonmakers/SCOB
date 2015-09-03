8cfa2de84abbad079e41fd29093b97d46aef4b63
RobertCL <RobertCL@iname.com>
2015-09-03 10:56:04 +0100

    Update animation order to stand inbetween moves to make it easier to tell which pattern is running.

--- software/firmware/SCOB/SCOB.ino
+++ software/firmware/SCOB/SCOB.ino
@@ -1,11 +1,11 @@
 #include <Servo.h>
-#include <NewPing.h>
+//#include <NewPing.h>
 #include "ServoAnimator.h"
 #include "Config.h"
 #include "Animations.h"
 
 // Objects
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+//NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 ServoAnimator anim(NUM_JOINTS);
 
 // Animation state - used to switch between different animations
@@ -22,7 +22,7 @@ void setup() {
   }
 
   //anim.moveServosTo((const byte*) walkForwardKeyframes, 1000);
-  anim.setAnimation(&walkForward);
+  anim.setAnimation(&stand);
 }
 
 
@@ -33,23 +33,39 @@ void loop() {
   if (!anim.isBusy()) {
       switch(state) {
           case 0:
-          anim.setAnimation(&turnLeft);
-          anim.setRepeatCount(2); // turn left total of 3 times
-          break;
+          anim.setAnimation(&stand);
+          anim.setRepeatCount(2);
 
           case 1:
           anim.setAnimation(&walkForward);
-          anim.setRepeatCount(4);
+          anim.setRepeatCount(2);
           break;
-
+          
           case 2:
-          anim.setAnimation(&turnRight);
-          anim.setRepeatCount(1);   // turn right twice
-          break;
+          anim.setAnimation(&stand);
+          anim.setRepeatCount(2);
 
           case 3:
+          anim.setAnimation(&turnLeft);
+          anim.setRepeatCount(2); // turn left total of 3 times
+          break;
+          
+          case 4:
+          anim.setAnimation(&stand);
+          anim.setRepeatCount(2);
+
+          case 5:
           anim.setAnimation(&walkForward);
-          anim.setRepeatCount(4);
+          anim.setRepeatCount(2);
+          break;
+          
+          case 6:
+          anim.setAnimation(&stand);
+          anim.setRepeatCount(2);
+
+          case 7:
+          anim.setAnimation(&turnRight);
+          anim.setRepeatCount(2);   // turn right twice
           break;
       }
       state++;
