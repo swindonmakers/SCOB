19904dd5317ff720dfd023b0d2dc1653192a4536
RobertCL <RobertCL@iname.com>
2015-11-03 14:28:51 +0000

    Update wait time to 30s before going back into random modes

--- software/firmware/SCOB_fpsm/SCOB_fpsm.ino
+++ software/firmware/SCOB_fpsm/SCOB_fpsm.ino
@@ -225,8 +225,8 @@ void activity_commandMode() {
       doCommand(cmdQ.dequeue());
       lastCommandEnd = 0;
     }
-    else if (millis() - lastCommandEnd > 5000) {
-      // exit command mode after 5s of no more commands
+    else if (millis() - lastCommandEnd > 30000) {
+      // exit command mode after 30s of no more commands
       do_explore();
     }
   }
